f21392935a3ce969ff54ff3748ac2b7d2638541f
LORDCOMMANDER <singhdeependra4630@gmail.com>
2025-09-16 14:01:20 +0530

    recursive method

--- striver c++/linkedList/sortLL.cpp
+++ striver c++/linkedList/sortLL.cpp
@@ -1,53 +1,57 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Node{
-    public:
+
+class Node {
+public:
     int data;
     Node* next;
-    Node(int data1){
-        data=data1;
-        next=nullptr;
+    Node(int data1) {
+        data = data1;
+        next = nullptr;
     }
 };
-Node* convertll(vector<int> &arr){
-    Node* head=new Node(arr[0]);
-    Node* temp=head;
-    for(int i=1;i<arr.size();i++){
-        Node* n=new Node(arr[i]);
-        temp->next=n;
-        temp=n;
+
+Node* convertll(vector<int> &arr) {
+    Node* head = new Node(arr[0]);
+    Node* temp = head;
+    for (int i = 1; i < arr.size(); i++) {
+        Node* n = new Node(arr[i]);
+        temp->next = n;
+        temp = n;
     }
     return head;
 }
-Node* sortll(Node* head){
+
+Node* sortll(Node* head) {
     vector<int> arr;
-    Node* temp=head;
-    while(temp!=NULL){
+    Node* temp = head;
+    while (temp != NULL) {
         arr.push_back(temp->data);
-        temp=temp->next;
+        temp = temp->next;
     }
-    sort(arr.begin(),arr.end());
-    int i=0;
-    temp=head;
-    while(temp!=NULL){
-        temp->data=arr[i];
-        i++;
-        temp=temp->next;
+    sort(arr.begin(), arr.end());
+    int i = 0;
+    temp = head;
+    while (temp != NULL) {
+        temp->data = arr[i++];
+        temp = temp->next;
     }
     return head;
 }
-void traverse(Node* head){
-    Node* temp=head;
-    while(temp!=NULL){
-        cout<<temp->data<<" ";
-        temp=temp->next;
+
+void traverse(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
     }
-    cout<<endl;
+    cout << endl;
 }
+
 Node* reverse(Node* head) {
     Node* prev = NULL;
     Node* curr = head;
-    while(curr != NULL) {
+    while (curr != NULL) {
         Node* nextNode = curr->next;
         curr->next = prev;
         prev = curr;
@@ -55,43 +59,44 @@ Node* reverse(Node* head) {
     }
     return prev;
 }
-Node* add1(Node* head) {
-    if(head==NULL || head->next==NULL) return head;
-    head = reverse(head);
-    Node* temp = head;
-    int carry = 1;
 
-    while(temp != NULL) {
-        temp->data += carry;
-        if(temp->data < 10) {
-            carry = 0;
-            break;
-        } else {
-            temp->data = 0;
-            carry = 1;
-        }
-        temp = temp->next;
-    }
-    if(carry == 1) {
+// Helper function to recursively add 1
+int helper(Node* head) {
+    if (head == NULL) return 1; // carry = 1 at the end
+    int carry = helper(head->next);
+    head->data += carry;
+    if (head->data < 10) return 0; // no carry
+    head->data = 0;
+    return 1; // still carry
+}
+
+Node* add1(Node* head) {
+    if (head == NULL) return nullptr;
+
+    int carry = helper(head);
+
+    if (carry == 1) {
         Node* newnode = new Node(1);
-        head=reverse(head);
         newnode->next = head;
         head = newnode;
     }
-    head = reverse(head);
     return head;
 }
-int main(){
-    vector<int> arr={6,4,7,8,2,9,1};
-    Node* head=convertll(arr);
+
+int main() {
+    vector<int> arr = {6, 4, 7, 8, 2, 9, 1};
+    Node* head = convertll(arr);
+
+    cout << "Original list: ";
     traverse(head);
-    cout<<endl;
-    cout<<"sorting the SLL"<<endl;
+
+    cout << "Sorting the SLL:" << endl;
     sortll(head);
     traverse(head);
-    cout<<endl;
-    cout<<"adding 1 into the linked list"<<endl;
-    add1(head);
+
+    cout << "Adding 1 to the linked list:" << endl;
+    head = add1(head);
     traverse(head);
+
     return 0;
 }
