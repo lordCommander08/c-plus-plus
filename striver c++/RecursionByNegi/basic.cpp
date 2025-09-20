b7c5bd7ddae33b466d5a0dff4d70b290348c23ea
LORDCOMMANDER <singhdeependra4630@gmail.com>
2025-09-20 23:27:20 +0530

    finshing basic folder

--- striver c++/RecursionByNegi/basic.cpp
+++ striver c++/RecursionByNegi/basic.cpp
@@ -79,18 +79,59 @@
 //     cout<<minofarray(arr,5);
 //     return 0; 
 // }
-#include<bits/stdc++.h>
-using namespace std;
-void rev(string &str,int start,int end){
-    if(start>=end){
-        return;
-    }
-    swap(str[start],str[end]);
-    rev(str,start+1,end-1);
-}
-int main(){
-    string s="Deependra";
-    rev(s,0,s.size()-1);
-    cout<<s;
-    return 0;
-}
+// #include<bits/stdc++.h>
+// using namespace std;
+// void rev(string &str,int start,int end){
+//     if(start>=end){
+//         return;
+//     }
+//     swap(str[start],str[end]);
+//     rev(str,start+1,end-1);
+// }
+// int main(){
+//     string s="Deependra";
+//     rev(s,0,s.size()-1);
+//     cout<<s;
+//     return 0;
+// }
+// #include<bits/stdc++.h>
+// using namespace std;
+// bool linearsearch(int arr[],int index,int n,int x){
+//     if(n==0){
+//         return false;
+//     }
+//     if(arr[index]==x){
+//         return true;
+//     }
+//     return linearsearch(arr,index+1,n-1,x);
+// }
+// int main(){
+//     int arr[5]={1,2,3,4,5};
+//     int n;
+//     cout<<"enter the number you want to search"<<endl;
+//     cin>>n;
+//     cout<<linearsearch(arr,0,5,n);
+//     return 0;
+// }
+// #include<bits/stdc++.h>
+// using namespace std;
+// bool binarysearch(int arr[],int low,int high,int x){
+//     if(low>high) return false;
+//     int mid=(low+high)/2;
+//     if(arr[mid]==x) return true;
+//     if(arr[mid]>x){
+//         return binarysearch(arr,low,mid-1,x);
+//     }
+//     return binarysearch(arr,mid+1,high,x);
+// }
+// int main(){
+//     int arr[]={1,3,7,89,8,43,23,25};
+//     int high = sizeof(arr)/sizeof(arr[0]) - 1;
+//     int low=0;
+//     cout<<"enter the element u want to search"<<" ";
+//     int key;
+//     cin>>key;
+//     int a=binarysearch(arr,low,high,key);
+//     cout<<a;
+//     return 0;
+// }
