e012a2f3b8d9509d16038be52c0ca03dff3f712f
LORDCOMMANDER <singhdeependra4630@gmail.com>
2025-09-27 13:42:55 +0530

    targetSum Set

---
+++ striver c++/RecursionByNegi/settargetsum.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+void settargetsum(vector<int> &nums,set<vector<int>> &ans,vector<int> &temp,int index,int n,int sum,int target){
+    if(index==n){
+        if(sum==target){
+            ans.insert(temp);
+        }
+        return;
+    }
+    settargetsum(nums,ans,temp,index+1,n,sum,target);
+    temp.push_back(nums[index]);
+    settargetsum(nums,ans,temp,index+1,n,sum+nums[index],target);
+    temp.pop_back();
+}
+int main(){
+
+    vector<int> nums={3,4,7};
+    int n=nums.size();
+    set<vector<int>> ans;
+    vector<int> temp;
+    settargetsum(nums,ans,temp,0,n,0,7);
+    for(auto &x:ans){
+        for(auto y:x){
+            cout<<y<<" ";
+        }
+        cout<<endl;
+    }
+}
