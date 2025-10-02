b9784822c4532b50e44caaf13eac115b35f2f4bd
LORDCOMMANDER <singhdeependra4630@gmail.com>
2025-10-02 16:33:49 +0530

    update

--- striver c++/bit manipulation/primefactor.cpp
+++ striver c++/bit manipulation/primefactor.cpp
@@ -29,12 +29,13 @@ int main(){
     int n;
     cin>>n;
     vector<int> ans;
-    for(int i=2;i<=n;i++){
+    for(int i=2;i<=sqrt(n);i++){
         if(n%i==0) ans.push_back(i);
         while(n%i==0){
             n=n/i;
         }
     }
+    if(n!=1) ans.push_back(n);
     for(auto it: ans){
         cout<<it<<" ";
     }
