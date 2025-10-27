97fc0f780fa3c83bf59a874fa81e0dc8f7931a79
LORDCOMMANDER <singhdeependra4630@gmail.com>
2025-10-27 13:33:22 +0530

    optimal approach

--- striver c++/stack&queue/CelebrityProblem.cpp
+++ striver c++/stack&queue/CelebrityProblem.cpp
@@ -1,24 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
 int findCelebrity(vector<vector<int>> &mat){
-    int n=mat.size();
-    vector<int> knowme(n);
-    vector<int> iknow(n);
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(mat[i][j]==1){
-            knowme[j]++;
-            iknow[i]++;
-            }
-        }
+    // brute Approach
+//     int n=mat.size();
+//     vector<int> knowme(n);
+//     vector<int> iknow(n);
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<n;j++){
+//             if(mat[i][j]==1){
+//             knowme[j]++;
+//             iknow[i]++;
+//             }
+//         }
+//     }
+//     for(int i=0;i<n;i++){
+//         if(knowme[i]==n-1 && iknow[i]==0){
+//             return i;
+//         }
+//     }
+//     return -1;
+ 
+//  optimal Approach
+int n=mat.size();
+int top=0;
+int bottom=n-1;
+while(top<bottom){
+    if(mat[top][bottom]==1){
+        top++;
     }
-    for(int i=0;i<n;i++){
-        if(knowme[i]==n-1 && iknow[i]==0){
-            return i;
-        }
-    }
-    return -1;
+    else bottom--;
 }
+if(top>bottom) return -1;
+int celeb=top;
+for(int i=0;i<n;i++){
+    if(i==celeb) continue;
+    else if(mat[celeb][i]==1 || mat[i][celeb]==0) return -1;
+}
+return top;
+ }
 int main(){
     vector<vector<int>> mat={
         {0,1,1,0},
