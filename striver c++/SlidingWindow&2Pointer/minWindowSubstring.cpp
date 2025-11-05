6c978376a616be213e113228cc21dff3ff491007
LORDCOMMANDER <singhdeependra4630@gmail.com>
2025-11-05 12:12:43 +0530

    updated

--- striver c++/SlidingWindow&2Pointer/minWindowSubstring.cpp
+++ striver c++/SlidingWindow&2Pointer/minWindowSubstring.cpp
@@ -1,29 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 string WindowS(string &s,string &t){
-    int n=s.size(),m=t.size();
-    int minlen=1e9;
-    int sindex=-1;
-    for(int i=0;i<n;i++){
-        unordered_map<char,int> mp;
-        for(int j=0;j<m;j++) mp[t[j]]++;
-        int count=0;
-        for(int j=i;j<n;j++){
-            if(mp[s[j]]>0) {
-                count=count+1;
-            }
-            mp[s[j]]--;
-            if(count==m){
-                if(j-i+1 < minlen){
-                    minlen=j-i+1;
-                    sindex=i;
-                }
-                break;
-            }
+     int n = s.size(), m = t.size();
+        if (m > n) return "";
+
+        int minlen = 1e9;
+        int sindex = -1;
+
+        unordered_map<char, int> mp;
+        for (int i = 0; i < m; i++) {
+            mp[t[i]]++;
         }
-    }
-    if(sindex==-1) return "";
-    return s.substr(sindex,minlen);
+
+        int l = 0, r = 0;
+        int count = 0;
+        int required = m;
+
+        while (r < n) {
+            if (mp[s[r]] > 0)
+                count++;
+            mp[s[r]]--;
+
+            while (count == required) {
+                if (r - l + 1 < minlen) {
+                    minlen = r - l + 1;
+                    sindex = l;
+                }
+                mp[s[l]]++;
+                if (mp[s[l]] > 0)
+                    count--;
+                l++;
+            }
+            r++;
+        }
+
+        if (sindex == -1) return "";
+        return s.substr(sindex, minlen);
 }
 int main(){
     string s="ddaaabbca";
