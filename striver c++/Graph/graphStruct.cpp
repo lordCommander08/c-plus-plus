49f7acfe6444a5b476812684e3572fac13276263
LORDCOMMANDER <singhdeependra4630@gmail.com>
2026-02-16 22:34:36 +0530

    graph structure with BFS

--- striver c++/Graph/graphStruct.cpp
+++ striver c++/Graph/graphStruct.cpp
@@ -8,7 +8,7 @@ class Graph{
         v=vertices;
         adj.resize(v);
     }
-    void addEdge(int u, int v, bool direction){
+    void addEdge(int u,int v,bool direction){
         adj[u].push_back(v);
         if(direction==false){
             adj[v].push_back(u);
@@ -24,20 +24,43 @@ class Graph{
         }
     }
 };
-int main(){
-    int v,e;
-    cout<<"enter the no of vertices -> "<<endl;
-    cin>>v;
-    cout<<"enter the no of edges ->"<<endl;
-    cin>>e;
-    Graph g(v);
-    cout << "Enter edges (u v):" << endl;
-    for(int i = 0; i < e; i++) {
-        int u, v;
-        cin >> u >> v;
-        g.addEdge(u, v,false);
+vector<int> bfsG(int v,vector<vector<int>>& adj){
+    vector<int> vis(v,0);
+    queue<int> q;
+    vis[0]=1;
+    q.push(0);
+    vector<int> bfs;
+    while(!q.empty()){
+        int node=q.front();
+        q.pop();
+        bfs.push_back(node);
+        for(auto it:adj[node]){
+            if(!vis[it]){
+                vis[it]=1;
+                q.push(it);
+            }
+        }
     }
-    cout<<"Graph Representation\n";
+    return bfs;
+}
+int main(){
+    int n,m;
+    cout<<"enter the number of vertices = ";
+    cin>>n;
+    cout<<"enter the number of edges = ";
+    cin>>m;
+    cout << "Enter edges (u v):" << endl;
+    Graph g(n);
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>> v;
+        g.addEdge(u,v,0);
+    }
     g.printGraph();
+    vector<int> result = bfsG(n, g.adj);
+    cout << "BFS Traversal: ";
+    for(int node : result){
+        cout << node << " ";
+    }
     return 0;
 }
