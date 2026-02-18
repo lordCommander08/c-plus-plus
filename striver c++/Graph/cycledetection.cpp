b451b3f3ec9dca01055bc038cc359f4197a7feec
LORDCOMMANDER <singhdeependra4630@gmail.com>
2026-02-18 17:44:40 +0530

    cycle detection

---
+++ striver c++/Graph/cycledetection.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    bool detect(int src, vector<vector<int>>& adj, vector<int>& vis) {
+        vis[src] = 1;
+
+        queue<pair<int,int>> q;
+        q.push({src, -1});
+
+        while(!q.empty()) {
+            int node = q.front().first;
+            int parent = q.front().second;
+            q.pop();
+
+            for(auto adjacent : adj[node]) {
+                if(!vis[adjacent]) {
+                    vis[adjacent] = 1;
+                    q.push({adjacent, node});
+                }
+                else if(adjacent != parent) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+public:
+    bool isCycle(int V, vector<vector<int>>& edges) {
+
+        vector<vector<int>> adj(V);
+
+        for(auto edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<int> vis(V, 0);
+
+        for(int i = 0; i < V; i++) {
+            if(!vis[i]) {
+                if(detect(i, adj, vis))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+int main() {
+
+    int V, E;
+    cin >> V;
+    cin >> E;
+
+    vector<vector<int>> edges(E, vector<int>(2));
+
+    for(int i = 0; i < E; i++) {
+        cin >> edges[i][0] >> edges[i][1];
+    }
+
+    Solution obj;
+
+    if(obj.isCycle(V, edges))
+        cout << "true";
+    else
+        cout << "false";
+
+    return 0;
+}
