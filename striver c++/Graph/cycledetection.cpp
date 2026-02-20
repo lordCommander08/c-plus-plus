b7abc4e958ee5640bd17e6fa672cf864499966d5
LORDCOMMANDER <singhdeependra4630@gmail.com>
2026-02-20 09:54:18 +0530

    changes

--- striver c++/Graph/cycledetection.cpp
+++ striver c++/Graph/cycledetection.cpp
@@ -1,19 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 class Solution {
 private:
     bool detect(int src, vector<vector<int>>& adj, vector<int>& vis) {
         vis[src] = 1;
-
         queue<pair<int,int>> q;
         q.push({src, -1});
-
         while(!q.empty()) {
             int node = q.front().first;
             int parent = q.front().second;
             q.pop();
-
             for(auto adjacent : adj[node]) {
                 if(!vis[adjacent]) {
                     vis[adjacent] = 1;
@@ -24,32 +20,24 @@ private:
                 }
             }
         }
-
         return false;
     }
-
 public:
     bool isCycle(int V, vector<vector<int>>& edges) {
-
         vector<vector<int>> adj(V);
-
         for(auto edge : edges) {
             int u = edge[0];
             int v = edge[1];
-
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-
         vector<int> vis(V, 0);
-
         for(int i = 0; i < V; i++) {
             if(!vis[i]) {
                 if(detect(i, adj, vis))
                     return true;
             }
         }
-
         return false;
     }
 };
