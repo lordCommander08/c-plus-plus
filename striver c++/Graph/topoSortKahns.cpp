99d7d9ac8ef3b56c16783821df00eac4b7413a67
LORDCOMMANDER <singhdeependra4630@gmail.com>
2026-02-21 11:50:22 +0530

    topological sorting

---
+++ striver c++/Graph/topoSortKahns.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    vector<int> topoSort(int V, vector<vector<int>>& adj) {
+        vector<int> indegree(V,0);
+        for(int i=0;i<V;i++){
+            for(auto it:adj[i]){
+                indegree[it]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int>topo;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        return topo;
+    }
+};
+int main(){
+    int V=6;
+    vector<vector<int>> adj = {
+    {1, 2},
+    {3},     
+    {4},      
+    {5},      
+    {5},      
+    {}        
+};
+Solution g;
+vector<int> res=g.topoSort(V,adj);
+for(int x:res){
+    cout<<x<<" ";
+}
+return 0;
+}
