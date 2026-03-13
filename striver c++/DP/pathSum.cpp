ddd513237f05a89d4a94bcac41566c43cd336c0b
LORDCOMMANDER <singhdeependra4630@gmail.com>
2026-03-13 14:30:03 +0530

    no change

--- striver c++/DP/pathSum.cpp
+++ striver c++/DP/pathSum.cpp
@@ -1,18 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
-int minPath(vector<vector<int>>&nums,int m, int n,vector<vector<int>>&dp){
-            if (m==0 && n==0) return nums[m][n];
-            if (m<0 || n<0) return INT_MAX;
-            if(dp[m][n]!=-1) return dp[m][n];
-                int up = minPath(nums,m-1,n,dp);
-                int left = minPath(nums,m,n-1,dp);
-                int ans = nums[m][n] + min(up, left);
-                return dp[m][n]=ans;
+
+int minPath(vector<vector<int>>& nums, int m, int n){
+
+    vector<vector<int>> dp(m, vector<int>(n,0));
+
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+
+            if(i==0 && j==0)
+                dp[i][j] = nums[i][j];
+
+            else{
+                int up = INT_MAX, left = INT_MAX;
+
+                if(i>0) up = nums[i][j] + dp[i-1][j];
+                if(j>0) left = nums[i][j] + dp[i][j-1];
+
+                dp[i][j] = min(up,left);
+            }
+        }
+    }
+
+    return dp[m-1][n-1];
 }
 int main(){
     vector<vector<int>> nums = {{1,3,1},{1,5,1},{4,2,1}};
-    int m=3,n=3;
-    vector<vector<int>> dp(m,vector<int>(n,-1));
-    cout<<minPath(nums,m-1,n-1,dp);
+
+    int m = nums.size();
+    int n = nums[0].size();
+
+    cout<<minPath(nums,m,n);
+
     return 0;
 }
