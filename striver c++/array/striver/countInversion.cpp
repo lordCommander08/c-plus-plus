bba83e6c144ba5a6a5769af2a05c2abc78c8f9eb
Deependra Gurjar <singhdeependra4630@gmail.com>
2026-06-14 12:29:30 +0530

    count inversion

--- striver c++/array/striver/countInversion.cpp
+++ striver c++/array/striver/countInversion.cpp
@@ -1,43 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
-int cnt=0;
-void mergeArray(vector<int>& arr,int low,int mid,int high){
+long long mergeArray(vector<int>& arr,int low,int mid,int high){
     vector<int> temp;
     int left=low,right=mid+1;
-    while(left<=mid&&right<=high){
+    long long cnt=0;
+    while(left<=mid && right<=high){
         if(arr[left]<=arr[right]){
             temp.push_back(arr[left++]);
         }
         else{
             temp.push_back(arr[right++]);
+            cnt+=(mid-left+1);
         }
     }
     while(left<=mid) temp.push_back(arr[left++]);
     while(right<=high) temp.push_back(arr[right++]);
+
     for(int i=low;i<=high;i++){
         arr[i]=temp[i-low];
     }
+    return cnt;
 }
 
-void countPairs(vector<int>& nums,int low,int mid,int high){
-    int right=mid+1;
-    for(int i=low;i<=mid;i++){
-        while(right<=high&&nums[i]>2LL*nums[right]) right++;
-        cnt+=right-(mid+1);
-    }
-}
-
-void mergeSort(vector<int>& arr,int low,int high){
-    if(low>=high) return;
+long long mergeSort(vector<int>& arr,int low,int high){
+    if(low>=high) return 0;
     int mid=low+(high-low)/2;
-    mergeSort(arr,low,mid);
-    mergeSort(arr,mid+1,high);
-    countPairs(arr,low,mid,high);
-    mergeArray(arr,low,mid,high);
+    long long cnt=0;
+    cnt+=mergeSort(arr,low,mid);
+    cnt+=mergeSort(arr,mid+1,high);
+    cnt+=mergeArray(arr,low,mid,high);
+    return cnt;
 }
 int main(){
     vector<int> nums={5,3,2,4,1};
-    mergeSort(nums,0,nums.size()-1);
-    cout<<cnt;
+    cout<<mergeSort(nums,0,nums.size()-1);
     return 0;
 }
